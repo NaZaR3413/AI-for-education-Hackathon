fdbfd70c2fc957556ac71e1ba8deb24a89cab0fb
Nilay Patel <nilayno1@gmail.com>
2024-04-01 18:13:09 -0700

    pre link integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./styles.scss";
 import CustomNav from "./navbar-components/navbar.js";
+import { Route, Routes } from "react-router-dom"
 import Home from "./pages/home.js"
 
 export default function App() {
   return (
     <CustomNav
       li={[
-        ["Dashboard", "img/dashboard.svg"],
+        ["Home", "img/dashboard.svg", "./pages/home"],
         ["Restautant’s", "img/restaurant.svg"],
         ["Manage User’s", "img/manage user.svg"],
         ["Manage Order’s", "img/manage  order.svg"],
