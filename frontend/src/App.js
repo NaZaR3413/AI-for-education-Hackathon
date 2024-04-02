91fe9a8b24b96b12856ab5beccfa998986292f31
Nilay Patel <nilayno1@gmail.com>
2024-04-01 20:38:38 -0700

    results/routing updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./styles.scss";
 import CustomNav from "./navbar-components/navbar.js";
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/home.js"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Results from "./pages/results";
 
 export default function App() {
   return (
-    <CustomNav
-      li={[
-        ["Home", "img/dashboard.svg", "./pages/home"],
-        ["Restautant’s", "img/restaurant.svg"],
-        ["Manage User’s", "img/manage user.svg"],
-        ["Manage Order’s", "img/manage  order.svg"],
-        ["Manage Coupon’s", "img/manage coupon.svg"]
-      ]}
-    />
+    <Router>
+      <CustomNav
+        li={[
+          ["Home", "img/home.png", "/home"],
+          ["Results", "img/home.png", "/results"],
+          // Update other items as necessary, ensuring you provide the correct paths
+        ]}
+      />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/results" element={<Results />} />
+        {/* Define other routes here */}
+      </Routes>
+    </Router>
   );
 }
--- frontend/src/navbar-components/navbar.js
+++ frontend/src/navbar-components/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const CustomNav = ({ li }) => {
   const [window, setWindow] = useState(false);
@@ -11,44 +11,33 @@ const CustomNav = ({ li }) => {
       setWindow(false);
     }
   };
+
   return (
     <nav className="navbar-menu" style={{ width: window === false ? 250 : 60 }}>
-      <div className="burger" onClick={() => openClose()}>
+      <div className="burger" onClick={openClose}>
         <img src="img/menu.svg" alt="burger" />
       </div>
       <ul className="navbar__list">
-        {li.map((item, i) => (
-          <div className="navbar__li-box" key={i}>
-            <img
-              src={item[1]}
-              alt={item[1]}
-              style={{ paddingLeft: window === false ? 27 : 17 }}
-            />
-            <li
-              className="navbar__li"
-              style={{ display: window === false ? "inline-block" : "none" }}
-            >
-              {item[0]}
-            </li>
+        {li.map((item, index) => (
+          <div className="navbar__li-box" key={index}>
+            <Link to={item[2] || "/"} style={{ display: "flex", alignItems: "center" }}>
+              <img viewBox="0 0 41 28" width="41" height="28"
+                src={item[1]}
+                alt={item[0]}
+                style={{ paddingLeft: window === false ? 27 : 17 }}
+              />
+              <li
+                className="navbar__li"
+                style={{ display: window === false ? "inline-block" : "none" }}
+              >
+                {item[0]}
+              </li>
+            </Link>
           </div>
         ))}
       </ul>
     </nav>
   );
 };
-
-
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  )
-}
 
 export default CustomNav;
